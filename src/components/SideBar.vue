29fc1ea7c9607207896dbdca7ae84004464ef372
aliraza114 <aliraza3569@gmail.com>
2020-10-07 11:31:32 +0500

    first commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+import DxChart from 'devextre'
+export default {
+  name: 'SideBar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
